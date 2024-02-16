05908ac8f42cdbb26af6ab4ca39999d39bd10847
ABDEL HAKEM ZAYAN <abdozayan12@gmail.com>
2024-02-16 15:06:44 +0200

    fix camel case linter error

--- src/component/weatherApp/WeatherApp.jsx
+++ src/component/weatherApp/WeatherApp.jsx
@@ -1,30 +1,30 @@
 import React from 'react';
 import './weatherApp.css';
 
-import search_icon from '../assets/search.png';
-import clear_icon from '../assets/clear.png';
-import cloud_icon from '../assets/cloud.png';
-import rain_icon from '../assets/rain.png';
-import snow_icon from '../assets/snow.png';
-import drizzle_icon from '../assets/drizzle.png';
-import humidity_icon from '../assets/humidity.png';
-import wind_icon from '../assets/wind.png';
+import searchIcon from '../assets/search.png';
+import clearIcon from '../assets/clear.png';
+import cloudIcon from '../assets/cloud.png';
+import rainIcon from '../assets/rain.png';
+import snowIcon from '../assets/snow.png';
+import drizzleIcon from '../assets/drizzle.png';
+import humidityIcon from '../assets/humidity.png';
+import windIcon from '../assets/wind.png';
 
 const WeatherApp = () => (
   <div className="container">
     <div className="top-section">
       <input type="text" placeholder="Search for places" className="CityInput" />
       <div className="search-icon">
-        <img src={search_icon} alt="search_icon" />
+        <img src={searchIcon} alt="searchIcon" />
       </div>
     </div>
-    <div className='weather-icon'>
-        <img src={cloud_icon} alt='cloud_icon' />
-        </div>
-        <div className='weather-info'>
-            <div className='temperature'>25°C</div>
-            <div className='weather'>Cloudy</div>
-          </div>
+    <div className="weather-icon">
+      <img src={cloudIcon} alt="cloudIcon" />
+    </div>
+    <div className="weather-info">
+      <div className="temperature">25°C</div>
+      <div className="weather">Cloudy</div>
+    </div>
   </div>
 );
 
